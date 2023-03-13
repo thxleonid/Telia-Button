7139b6a10e7811d2db0fc810a957539ff75faace
thxleonid <54886574+thxleonid@users.noreply.github.com>
2023-03-13 11:09:00 +0200

    TS

--- src/components/OptionsList.tsx
+++ src/components/OptionsList.tsx
@@ -5,12 +5,14 @@ import React from 'react'
 const OptionsList: 
     React.FunctionComponent<{options: OptionParams[]; handleOptionSelection: (option: OptionParams) => void;}> = ({ options, handleOptionSelection }) => {
     
-    return <>{options.map((option, index) =>
-        <OptionElement
-            option={option}
-            key={index}
-            handleOptionSelection={handleOptionSelection}
-        />)}</>
+    return  <>
+            {options.map((option, index) =>
+                <OptionElement
+                    option={option}
+                    key={index}
+                    handleOptionSelection={handleOptionSelection}
+                />)}
+            </>
 }
 
 export default OptionsList;
--- src/enum.ts
+++ src/enum.ts
@@ -1,20 +1,8 @@
-export enum OptionElementStates {
-    ACTIVE = 'option--active',
-    DISABLED = 'option--disabled',
-    SUPPORT = 'option--support',
-    REGULAR = 'option--regular',
-}
-
 export enum ButtonStates {
     DROPDOWN_OPEN = 'btn--dropdown-open',
     ENABLED = "btn--enabled",
     DISABLED = "btn--disabled"
 }
-
-export enum IconAlign {
-    LEFT = 'left',
-    RIGHT = 'right'
-} 
 
 export enum ColorScheme {
     TELIA = 'telia',
@@ -22,3 +10,15 @@ export enum ColorScheme {
     ELISA = 'elisa',
     TELE2 = 'tele2'
 } 
+
+export enum IconAlign {
+    LEFT = 'left',
+    RIGHT = 'right'
+} 
+
+export enum OptionElementStates {
+    ACTIVE = 'option--active',
+    DISABLED = 'option--disabled',
+    SUPPORT = 'option--support',
+    REGULAR = 'option--regular',
+}
